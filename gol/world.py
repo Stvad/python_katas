d673356f68ba03326912659f4cab61ae59969b69
Vladyslav Sitalo <sitalov@amazon.com>
2019-01-20 17:42:16 +0000

    World serialization

--- gol/world.py
+++ gol/world.py
@@ -1,11 +1,13 @@
-# todo desirealization?
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Dict, Type
 
 from functional import seq
 
-from gol.cell import Cell
+from gol.cell import Cell, AliveCell, DeadCell
 from gol.point import Point
+
+representations: Dict[str, Type] = {'*': AliveCell, '.': DeadCell}
+reversed_representations = {v: k for k, v in representations.items()}
 
 
 @dataclass
@@ -22,3 +24,18 @@ class World:
         return self.data and \
                coordinate.y in range(len(self.data)) and \
                coordinate.x in range(len(self.data[0]))
+
+    def __repr__(self):
+        return '\n'.join([World.serialize_line(line) for line in self.data])
+
+    @staticmethod
+    def from_string(world_string: str):
+        return World([World.deserialize_line(line) for line in world_string.split()])
+
+    @staticmethod
+    def deserialize_line(line):
+        return [representations[c]() for c in line]
+
+    @staticmethod
+    def serialize_line(line):
+        return ''.join([reversed_representations[type(cell)] for cell in line])
--- test/katas_test/world_spec.py
+++ test/katas_test/world_spec.py
@@ -1,6 +1,6 @@
 from expects import expect
-from expects.matchers.built_in import be
-from mamba import description, it
+from expects.matchers.built_in import be, equal
+from mamba import description, it, context
 
 from gol.cell import AliveCell, DeadCell
 from gol.point import Point
@@ -15,3 +15,14 @@ with description("World") as self:
 
     with it("should return an appropriate number of neighbors for cell in the middle"):
         expect(World([[a, d, d]]).count_living_neighbors_for(Point(0, 1))).to(be(1))
+
+    with context("serialization"):
+        with it("should be able to load world from string"):
+            expect(World.from_string(".*.").data).to(equal([[d, a, d]]))
+
+        with it("should be able to handle multiline strings"):
+            expect(World.from_string(".\n*").data).to(equal([[d], [a]]))
+
+        with it("should be able to do round-trip"):
+            world_map = "..*.\n.**.\n.*.."
+            expect(repr(World.from_string(world_map))).to(equal(world_map))
